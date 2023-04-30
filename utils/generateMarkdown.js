a3edf3a6b438c7f1e9e4c1fedd866f2fa043bd02
Meghan Simmons <simmons.meghan@gmail.com>
2023-04-30 17:43:47 -0400

    added function renderLicenseSection to add a message to the License section or return empty string

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -31,7 +31,15 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-// function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  for(let i = 0; i < arrayLicense.length; i++){
+    if(license == arrayLicense[3]){
+      return "";
+    }else{
+      return `This application is covered under the [${license}]`;
+    }
+  }
+}
 
 //function generateMarkdown generates a markdown for README
 function generateMarkdown(data) {
@@ -54,7 +62,7 @@ function generateMarkdown(data) {
   ## Usage
   ${data.usage}
   ## License
-  This application is covered under the [${data.license}]${renderLicenseLink(data.license)}.
+  ${renderLicenseSection(data.license)}${renderLicenseLink(data.license)}
   ## Contributing
   ${data.contributing}
   ## Tests
